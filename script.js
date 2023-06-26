c5fbbfaf4e3924e2a94168ecdfc72282b8bd0a92
PaoloArmentano <armentano.paolo@gmail.com>
2023-06-26 15:10:27 +0100

    updated js file

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 "use strict";
  console.log("Duck you");
 
- let allDucks = [];
+
+// global varaibles
+// querySelector returns the first element in the document that matches
+const productContainer = document.querySelector("div");
+const resultsButton = document.querySelector("section + div");
+const image1 = document.querySelector("section img:first-child");
+const image2 = document.querySelector("section img:nth-child(2)");
+const image3 = document.querySelector("section img:nth-child(3)");
+
+let clicks = 0;
+const maxClicksAllowed = 25;
+
+ let allProducts = [];
 
  function getRandomNumber() {
-    return Math.floor(Math.random() * allDucks.length);
+    return Math.floor(Math.random() * allProducts.length);
   }
   
-
-
- function Duck(name, src, views){
+ function Product(name, src, views){
     this.name = name;
     this.src = src;
     this.views = 0;
     this.clicks = 0;
-    allDucks.push(this);
+    allProducts.push(this);
+ }
+
+ function renderProducts() {
+    // we need to generate a number to reference the product we want to render onto the page
+    let product1 = getRandomNumber();
+    let product2 = getRandomNumber();
+    let product3 = getRandomNumber();
+
+   // now we have two random numbers lets set the attributes of our images in the document.
+   image1.src = allProducts[product1].src;
+   image2.src = allProducts[product2].src;
+   image1.alt = allProducts[product1].name;
+   image2.alt = allProducts[product2].name;
+   image3.src = allProducts[product1].src;
+   allProducts[product1].views++;
+   allProducts[product2].views++;
+   allProducts[product3].views++;
  }
